ee1589621368e6aea0430f5122b6bf7396242439
Rizwan Shaikh <rixshaikh7@gmail.com>
2024-12-03 08:34:44 +0530

    added changes

--- models/userModel.js
+++ models/userModel.js
@@ -24,20 +24,16 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre("save", async function(next) {
   const user = this;
-
   // Hash the password only if it has been modified (or is new)
   if(!user.isModified("password")) return next();
 
 try{
   // Hash password generation
   const salt = await bcrypt.genSalt(10);
-
   // Hash password
   const hashedPassword = await bcrypt.hash(user.password,)
-  
   // Override the plain password with hashed one
   user.password = hashedPassword
-
   next();
 }catch(err){
   return next(err)
@@ -49,7 +45,6 @@ userSchema.methods.comparePassword =async function(canidatePassword){
     // Use bcrypt to compare the provided password with the hashed password
     const isMatch = await bcrypt.compare(canidatePassword, this.password);
     return isMatch;
-
   }catch(err){
     throw err;
   }
